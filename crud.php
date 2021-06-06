ad3282de5f0b5b053b779eafeb366266a123cd34
Bagus Anugrah <bagus.anugrah71@gmail.com>
2021-06-06 11:35:16 +0700

    update database connection

--- crud.php
+++ crud.php
@@ -1,8 +1,8 @@
 <?php
 	//koneksi database
 	$server = "localhost:3306";
-	$user = "admin";
-	$pass = "admin";
+	$user = "root";
+	$pass = "";
 	$database = "fazztrack";
 
 	$koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
